cbdb324816ad17e83c56b888abd24b0f981becd7
Ganesh <ganeshrodge25@gmail.com>
2025-02-13 12:17:50 +0530

    remove farmdashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup'; // Create this component
 import { SoilAnalysisForm } from './components/SoilAnalysisForm';
 import { CropOverview } from './components/CropOverview';
 import { AgricultureManagement } from './components/AgricultureManagement';
-import { FarmDashboard } from './components/FarmDashboard';
 import Profile from './components/Profile';
 
 export default function App() {
@@ -14,7 +13,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} /> 
-        <Route path="/farmdashboard" element={<FarmDashboard />} />
         <Route path="/soilanalysisform" element={<SoilAnalysisForm />} />
         <Route path="/agriculturemanagement" element={<AgricultureManagement />} />
         <Route path="/cropoverview" element={<CropOverview />} />
